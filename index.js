92e802d212926530d875cba2aa78dd67a44a0eb8
Tavish <YoTavishTayal@gmail.com>
2022-05-11 20:43:39 +0530

    aji hello

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ function Sussy() {
     let hrs = date.getHours()
     let min = date.getMinutes()
     let sec = date.getSeconds()
-    document.getElementsByClassName('hrs')[0].innerHTML = hrs;
     document.getElementsByClassName('min')[0].innerHTML = min;
     document.getElementsByClassName('sec')[0].innerHTML = sec;
     let sensei = document.getElementsByClassName('sesi')[0]
-    if (hrs > 12) {
+    if (hrs >= 12) {
         sensei.innerHTML = 'PM'
     } else{
         sensei.innerHTML = 'AM'
     }
-
+    
     if (hrs > 12) {
-        hrs = hrs - 12
+        hrs = hrs - 12;
     }
+    
+    document.getElementsByClassName('hrs')[0].innerHTML = hrs;
+
 }
 setInterval(Sussy, 10)
