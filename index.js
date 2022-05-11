3cafecf7aa5148e1d79c6a9ef244724bc2c8377b
Tavish <YoTavishTayal@gmail.com>
2022-05-11 06:16:35 +0530

    hello

---
+++ index.js
@@ -0,0 +1,20 @@
+function Sussy() {
+    let date = new Date();
+    let hrs = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
+    document.getElementsByClassName('hrs')[0].innerHTML = hrs;
+    document.getElementsByClassName('min')[0].innerHTML = min;
+    document.getElementsByClassName('sec')[0].innerHTML = sec;
+    let sensei = document.getElementsByClassName('sesi')[0]
+    if (hrs > 12) {
+        sensei.innerHTML = 'PM'
+    } else{
+        sensei.innerHTML = 'AM'
+    }
+
+    if (hrs > 12) {
+        hrs = hrs - 12
+    }
+}
+setInterval(Sussy, 10)
